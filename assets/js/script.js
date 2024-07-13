9ed2b0111fe1c14f3bd201b40af1a47b71239b97
Diego <diegocmd20@gmail.com>
2024-07-13 02:25:43 -0400

    Almost done, just for link positioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,24 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+    $("#submit-button").click(function () {
+        alert("Tu mensaje ha sido recibido");
+    });
+    $(window).scroll(function () {
+        if ($("#navbar-section").offset().top > 100) {
+            $("#navbar-section").addClass("bg-dark");
+            $("#navbar-section").removeClass("bg-transparent");
+        } else {
+            $("#navbar-section").removeClass("bg-dark");
+        }
+    });
+    $("#scroll-button").click(function () {
+        let subOffset = $(".subtitle").offset().top;
+        let navbarHeight = $("#navbar-section").outerHeight();
+        $('html, body').animate({
+            scrollTop: subOffset - navbarHeight - 20
+        }, 500);
+    });
+});
